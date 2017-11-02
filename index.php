9019b453be1bc298453ef806152dad5fcad06303
chichang <chichang@cjjh.ptc.edu.tw>
2017-11-02 11:46:23 +0800

    基本語法與變數練習

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: admin
+ * Date: 2017/11/2
+ * Time: 上午 10:51
+ */
+
+//say hello world to browser
+echo '<h1>Hello world</h1>';
+
+$extra='Cjjh';
+$var="My website title is $extra";
+
+echo $var;
+?>
+
+<html>
+<head>
+    <meta charset="utf-8">
+    <title> <?php echo $extra; ?></title>
+</head>
+<body>
+
+</body>
+</html>
