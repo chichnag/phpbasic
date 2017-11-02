11b7591e9235ab36ebe75e42304f422113bf4b05
chichang <chichang@cjjh.ptc.edu.tw>
2017-11-02 13:27:54 +0800

    reformat code

--- index.php
+++ index.php
@@ -11,16 +11,31 @@ echo '<h1>Hello world</h1>';
 
 $extra='Cjjh';
 $var="My website title is $extra";
-
 echo $var;
-?>
 
+$x = $_GET['x'];
+$y = $_GET['y'];
+$result = $x + $y;
+
+?>
+<!DOCTYPE html>
 <html>
 <head>
     <meta charset="utf-8">
     <title> <?php echo $extra; ?></title>
 </head>
 <body>
+<p>
+
+
+<form action="index.php" method="GET">
+    <input name="x" type="Number" value="<?= $x; ?>">
+    +
+    <input name="y" type="Number" value="<?= $y; ?>">
+    <input type="submit" value="=">
+    <?= $result; ?>
+</form>
+
 
 </body>
 </html>
