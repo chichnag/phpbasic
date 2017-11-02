af4d0ac59a9197dd51486bbfc8e4f8600433b362
chichang <chichang@cjjh.ptc.edu.tw>
2017-11-02 15:12:17 +0800

    reformat code

--- index.php
+++ index.php
@@ -9,28 +9,77 @@
 //say hello world to browser
 echo '<h1>Hello world</h1>';
 
-$extra='Cjjh';
-$var="My website title is $extra";
-echo $var;
+$names = ['Bob', 'John', 'Tom'];
+/**for ($i = 0; $i < count($names); $i++){
+ * echo $names[i];*/
 
-$x = $_GET['x'];
-$y = $_GET['y'];
-$result = $x + $y;
+
+if (isset($_GET['x'])) {
+    $x = $_GET['x'];
+} else {
+    $x = 0;
+}
+
+if (isset($_GET['y'])) {
+    $y = $_GET['y'];
+} else {
+    $y = 0;
+}
+
+if (isset($_GET['method'])) {
+    $method = $_GET['method'];
+} else {
+    $method = "add";
+}
+
+
+if ($method == 'add') {
+    $result = $x + $y;
+} elseif ($method == 'sub') {
+    $result = $x - $y;
+} elseif ($method == 'times') {
+    $result = $x * $y;
+} elseif ($method == 'division') {
+    $result = $x / $y;
+} else {
+    $result = $x + $y;
+}
 
 ?>
+
 <!DOCTYPE html>
 <html>
 <head>
     <meta charset="utf-8">
-    <title> <?php echo $extra; ?></title>
+    <title></title>
 </head>
 <body>
-<p>
 
+<h1>陣列裡共有 <?= count($names) ?>個名字</h1>
+<h1>目前指定的名字是<?= $names[1] ?></h1>
+
+<p>
 
 <form action="index.php" method="GET">
     <input name="x" type="Number" value="<?= $x; ?>">
-    +
+    <select name="method" id="">
+        <option value="add" <?php if ($method == "add") {
+            echo "selected";
+        } ?>>+
+        </option>
+        <option value="sub" <?php if ($method == "sub") {
+            echo "selected";
+        } ?>>-
+        </option>
+        <option value="times" <?php if ($method == "times") {
+            echo "selected";
+        } ?>>*
+        </option>
+        <option value="division" <?php if ($method == "division") {
+            echo "selected";
+        } ?>>/
+        </option>
+    </select>
     <input name="y" type="Number" value="<?= $y; ?>">
     <input type="submit" value="=">
     <?= $result; ?>
